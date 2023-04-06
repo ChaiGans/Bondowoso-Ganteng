cd7e043213adf829fe7e04d825320fc722514745
ChaiGans <elbertchailes888@gmail.com>
2023-04-06 16:48:46 +0700

    summonjin done F03

--- builtin.py
+++ builtin.py
@@ -1,5 +1,11 @@
+import csv
 def length(list):
     count = 0
     for x in  list:
         count += 1
     return count
+
+def appendrow_user(user_name, password, roles):
+    with open("./user.csv", 'a', newline='') as file:
+        csvwriter = csv.writer(file, delimiter=';')
+        csvwriter.writerow([user_name, password, roles])
