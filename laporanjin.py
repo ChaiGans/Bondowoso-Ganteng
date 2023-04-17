c76a05e8a5706e53f8279ec89014b4d6057831e2
leaxz <98172741+thaliafahira@users.noreply.github.com>
2023-04-17 10:31:50 +0700

    bingung nyari malas rajinnya :(

--- laporanjin.py
+++ laporanjin.py
@@ -1,5 +1,4 @@
-# nyerah nyari malas rajin
-
+# bingung nyari malas rajin nya
 
 from builtin import csvreader
 
@@ -7,42 +6,47 @@ data_user = csvreader('user.csv')
 data_candi = csvreader('candi.csv')
 data_bahanbangunan = csvreader('bahan_bangunan.csv')
 
-def rajin (jin1,jin2):
-    if (jin1).lower() < (jin2).lower():
-        return (jin1)
-    elif (jin1).lower() > (jin2).lower():
-        return (jin2)
+def laporanjin ():
+    def rajin (jin1,jin2):
+        if (jin1).lower() < (jin2).lower():
+            return (jin1)
+        elif (jin1).lower() > (jin2).lower():
+            return (jin2)
 
-def malas (jin1,jin2):
-    if (jin1).lower() > (jin2).lower():
-        return (jin1)
-    elif (jin1).lower() < (jin2).lower():
-        return (jin2)
+    def malas (jin1,jin2):
+        if (jin1).lower() > (jin2).lower():
+            return (jin1)
+        elif (jin1).lower() < (jin2).lower():
+            return (jin2)
 
-jin = 0
-jin_pembangun = 0
-jin_pengumpul = 0
-for i in range (1000):
-    if data_user [i][2] == "jin_pembangun":
-        jin_pembangun += 1
-    elif data_user [i][2] == "jin_pengumpul":
-        jin_pengumpul += 1
-totaljin = jin_pembangun + jin_pengumpul
+    jin = 0
+    jin_pembangun = 0
+    jin_pengumpul = 0
+    for i in range (length(data_user)):
+        if data_user [i][2] == "jin_pembangun":
+            jin_pembangun += 1
+        elif data_user [i][2] == "jin_pengumpul":
+            jin_pengumpul += 1
+    totaljin = jin_pembangun + jin_pengumpul
+    
+# yang ini emang cuma 3 kolom kan ya?
+    pasir = data_bahanbangunan [1][2]
+    batu = data_bahanbangunan [2][2]
+    air = data_bahanbangunan [3][2]
 
-pasir = data_bahanbangunan [1][2]
-batu = data_bahanbangunan [2][2]
-air = data_bahanbangunan [3][2]
+    print (f"> Total Jin: {totaljin}")
+    print(f"> Total Jin Pengumpul: {jin_pengumpul}")
+    print{f"> Total Jin Pengumpul: {jin_pembangun}"}
+    if jin_pembangun == 0:
+        print(f"> Jin Terajin: -")
+        print(f"> Jin Termalas: -")
+    else:
+        print(f"> Jin Terajin: {}")
+        print(f"> Jin Termalas: {}")
+    print(f"> Jumlah Pasir: {pasir} unit")
+    print(f"> Jumlah Air: {air} unit")
+    print(f"> Jumlah Batu: {batu} unit")
 
-print (f"> Total Jin: {totaljin}")
-print(f"> Total Jin Pengumpul: {jin_pengumpul}")
-print{f"> Total Jin Pengumpul: {jin_pembangun}"}
-if jin_pembangun == 0:
-    print(f"> Jin Terajin: -")
-    print(f"> Jin Termalas: -")
-else:
-    print(f"> Jin Terajin: {}")
-    print(f"> Jin Termalas: {}")
-print(f"> Jumlah Pasir: {pasir} unit")
-print(f"> Jumlah Air: {air} unit")
-print(f"> Jumlah Batu: {batu} unit")
 
+    
+       
