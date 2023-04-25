ddc3f83b82a22bb2b55ef220cc29c3ce70267b04
ChaiGans <elbertchailes888@gmail.com>
2023-04-25 14:11:29 +0700

    shafa req hancurkancandi

--- F11_hancurkancandi.py
+++ F11_hancurkancandi.py
@@ -6,20 +6,28 @@ def hancur(idxhancur):
 
 
 def hancurkancandi():
-    ditemukan=False
-    IDCandi = int(input("Masukkan ID Candi: ")) 
-    for i in range(tempat_variable.neff_data_candi):
-        if (tempat_variable.data_candi[i][0] == str(IDCandi)):
-            ditemukan=True
-            konfirmasi = input(f"Apakah anda yakin ingin menghancurkan candi ID: {IDCandi} (Y/N)?")
-            if (konfirmasi == "Y" or konfirmasi == "N"):
-                if (konfirmasi == "Y"): 
-                    hancur(i)
-                    tempat_variable.neff_data_candi -= 1
-                else: 
-                    print("Candi tidak jadi dihancurkan")
-                    break
-            else:
-                print("Kode Salah")
-    if ditemukan == False :
+    ditemukan = False
+    while True:
+        IDCandi = input("Masukkan ID Candi: ")
+        for i in range(tempat_variable.neff_data_candi):
+            if tempat_variable.data_candi[i][0] == IDCandi:
+                ditemukan = True
+                konfirmasi = input(f"Apakah anda yakin ingin menghancurkan candi ID: {IDCandi} (Y/N)?")
+                system = True
+                while system == True:
+                    if konfirmasi == "Y" or konfirmasi == "N":
+                        if konfirmasi == "Y": 
+                            hancur(i)
+                            tempat_variable.neff_data_candi -= 1
+                            print("Candi dengan ID", IDCandi, "berhasil dihancurkan")
+                            system = False
+                        else: 
+                            print("Candi tidak jadi dihancurkan")
+                            system = False
+                    else:
+                        print("Pilihan", konfirmasi, "tidak tersedia. Silahkan masukkan pilihan yang sesuai.")
+                        konfirmasi = input(f"Apakah anda yakin ingin menghancurkan candi ID: {IDCandi} (Y/N)?")
+                break
+        if ditemukan:
+            break
         print("Tidak ada candi dengan ID tersebut.")
