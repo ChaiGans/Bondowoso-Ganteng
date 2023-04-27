ff9760ae84f895972434eb8342b557f775a08a14
ChaiGans <elbertchailes888@gmail.com>
2023-04-27 15:12:09 +0700

    .

--- F12_ayemberkokok.py
+++ F12_ayemberkokok.py
@@ -3,10 +3,11 @@ import tempat_variable
 # Fungsi yang bertujuan untuk menentukan pemenang, Bandung Bondowoso atau Roro Jonggrang
 def ayamberkokok():    
     print("Kukuruyuk..Kukuruyuk")
-
+    print()
     # Kondisi jika candi masih dibawah < 100 maka Roro Jonggrang menang
     if (tempat_variable.neff_data_candi < 101): 
         print("Jumlah Candi:", str((tempat_variable.neff_data_candi)-1))
+        print()
         print("Selamat, Roro Jonggrang Memenangkan Permainan")
         print("")
         print("*Bandung Bondowoso angry noise*")
@@ -15,6 +16,7 @@ def ayamberkokok():
     # Kondisi jika candi sudah 100 maka Bandung Bondowoso menang
     else: 
         print("Jumlah Candi:", tempat_variable.neff_data_candi-1)
+        print()
         print("Yah, Bandung Bondowoso Memenangkan Permainan!")
     
     exit() # Keluar Program
