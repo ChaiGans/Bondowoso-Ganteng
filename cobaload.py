66c4e2248eda498f59c10b8173235b97768fa64a
RayNoor0 <rayendraalthaf04@gmail.com>
2023-04-29 00:28:30 +0700

    comment save n load

--- cobaload.py
+++ cobaload.py
@@ -1,7 +1,11 @@
 import tempat_variable
 import typing
 
+
+# membuka csv sesuai dengan jenis dari nama_lokasi yang diberikan
 def bukacsv (jenis : str, nama_lokasi : str):
+
+    #jenis mewakili tipe data yang akan dibaca (user/candi/bahan)
     #bkanan mewakili indeks kolom terbesar
     if jenis == "user":
         list_dummy = [["0" for i in range(3)] for j in range(113)]
@@ -14,10 +18,14 @@ def bukacsv (jenis : str, nama_lokasi : str):
     if jenis == "bahan":
         list_dummy = [["0" for i in range(3)] for j in range(4)]
         neff_list_dummy = 0
-        bkanan = 2    
+        bkanan = 2
+
+    # membuka file
     f=open(nama_lokasi,'r')
     isi=str(f.readline())+"*"
     i=0
+    
+    #membaca file perbaris dan memisahkannya setiap ";"
     while(isi != "*"):
         titip = ""
         j=k=0
@@ -30,16 +38,18 @@ def bukacsv (jenis : str, nama_lokasi : str):
                 if(isi[k]!='\n'):
                     titip+=str(isi[k])
             k+=1
-        
         list_dummy[i][j] = titip
         i+=1
         isi=f.readline()+"*"
         neff_list_dummy = i
     f.close()
+
+    # mengembalikan hasil baca
     return list_dummy, neff_list_dummy
         
 
 def load():
+    # membuka csv dan memasukkannya ke data
     tempat_variable.data_user, tempat_variable.neff_data_user = bukacsv("user", tempat_variable.lokasi_file+"\\user.csv")
     tempat_variable.data_candi, tempat_variable.neff_data_candi = bukacsv("candi", tempat_variable.lokasi_file+"\\candi.csv")
     tempat_variable.data_bahanbangunan, tempat_variable.neff_data_bahanbangunan = bukacsv("bahan",tempat_variable.lokasi_file+"\\bahan_bangunan.csv")
--- cobasave.py
+++ cobasave.py
@@ -1,6 +1,8 @@
 import time,os,tempat_variable
 
+# program menulis csv di folder tujuan sesuai dengan jenis dan berisikan data
 def tuliscsv(jenis : str, tujuan : str , data : list, neff_data : int):
+    # membaca jenis
     if(jenis == "user"):
         tujuan = tujuan + "\\user.csv"
         bkanan = 2
@@ -11,6 +13,7 @@ def tuliscsv(jenis : str, tujuan : str , data : list, neff_data : int):
         tujuan = tujuan + "\\bahan_bangunan.csv"     
         bkanan = 2
 
+    #menulis data perbaris
     fw = open(tujuan,"w")
     titip = ""
     for i in range(neff_data):
@@ -22,21 +25,31 @@ def tuliscsv(jenis : str, tujuan : str , data : list, neff_data : int):
     fw.write(titip)
     fw.close()
 
+# program save terhubung dengan program pusat
+# program save mengambil masukan folder tujuan save dan menggunakan
+# prosedur tuliscsv untuk menuliskan ke file
 def save():
+
+    #mengambil masukan folder tujuan save
     lokasi_save=input("Masukkan nama folder: ")
     print("Saving...")
     time.sleep(1)
     print()
+    # mengecek keberadaan folder save
+    # dan membuatnya jika belum ada
     if not (os.path.isdir("save")):
         print("Membuat folder save...")
         time.sleep(1)
         os.makedirs("save")
+    # mengecek keberadaan folder tujuan
+    # dan membuatnya jika belum ada
     if not (os.path.isdir("save\\"+str(lokasi_save))):
         print("Membuat folder save/"+str(lokasi_save)+"...")
         print()
         time.sleep(1)
         os.makedirs("save\\"+lokasi_save)
 
+    #mengisi/mereplace file-file di folder tujuan
     nama_folder = lokasi_save
     lokasi_save=os.path.join("save",lokasi_save)
     tuliscsv("user",lokasi_save,tempat_variable.data_user,tempat_variable.neff_data_user)
